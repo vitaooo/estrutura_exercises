ff19e125b175a207daf2482927168144eba5e24c
Victor <victorhvercosag@gmail.com>
2025-06-06 20:37:31 -0300

    exercicio 31 de estrutura, com hash

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿
+const int tam = 5;
+aluno[] vetor = new aluno[tam];
+
+int Hash(int ch)
+{
+    return (ch % tam);
+}
+
+void InsereLinear(int n, string nm, string e, ref int qc)
+{
+    int pos = Hash(n);
+    while (vetor[pos].nota != 0)
+    {
+        pos++;
+        pos = pos % tam;
+        qc++;
+    }
+
+    vetor[pos].nota = n;
+    vetor[pos].nome = nm;
+    vetor[pos].email = e;
+}
+
+int RecuperaLinear(int n)
+{
+    int pos = Hash(n);
+    int qtd = 0;
+    while (vetor[pos].nota != n && qtd <= tam)
+    {
+        qtd++;
+        pos++;
+        pos = pos % tam;
+    }
+    if (qtd < tam)
+        return pos;
+    else
+        return -1;
+
+}
+
+void Inicializar()
+{
+    for (int i = 0; i < tam; i++)
+    {
+        vetor[i] = new aluno();
+    }
+}
+
+
+// Main
+Inicializar();
+string op = "0";
+int qtdCo1 = 0;
+while (op != "4")
+{
+    Console.WriteLine("MENU");
+    Console.WriteLine("1 - Inserir");
+    Console.WriteLine("2 - Recuperar");
+    Console.WriteLine("3 - informar");
+    Console.WriteLine("4 - sair");
+    op = Console.ReadLine();
+    if (op == "1")
+    {
+        Console.Write("Nota: ");
+        int nota = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+        InsereLinear(nota, nome, email, ref qtdCo1);
+    }
+    else if (op == "2")
+    {
+        Console.Write("Nota: ");
+        int nota = Convert.ToInt32(Console.ReadLine());
+        int pos = RecuperaLinear(nota);
+        if (pos == -1)
+        {
+            Console.WriteLine("Não encontrou!");
+        }
+        else
+        {
+            Console.WriteLine("Nome: " + vetor[pos].nome);
+            Console.WriteLine("Email: " + vetor[pos].email);
+        }
+
+    } else if (op == "3")
+        Console.WriteLine("Quantidade de colisões: " + qtdCo1);
+}
+
+
+
+class aluno
+{
+    public int nota;
+    public string nome, email;
+}
